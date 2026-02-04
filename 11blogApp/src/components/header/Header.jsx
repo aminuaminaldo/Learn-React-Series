ed4a216743a9b72dce104a86b5c359df34977a84
Aminu Aminaldo <90836602+aminuaminaldo@users.noreply.github.com>
2026-02-04 10:32:24 +0300

    testing
    here we goes

--- 11blogApp/src/components/header/Header.jsx
+++ 11blogApp/src/components/header/Header.jsx
@@ -18,6 +18,7 @@ function Header() {
   return (
     <header className="py-3 shadow bg-gray-500">
       <Container>
+        {/* this is for test */}
         <nav className="flex">
           <div className="mr-4">
             <Link to="/">
@@ -37,7 +38,7 @@ function Header() {
                     {item.name}
                   </button>
                 </li>
-              ) : null
+              ) : null,
             )}
             {authStatus && (
               <li>
